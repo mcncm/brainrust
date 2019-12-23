35352ba893b4cc4ee87117afd2f4937f7b170d9b
mcncm <33966432+mcncm@users.noreply.github.com>
2019-12-23 01:53:56 -0500

    Stepping through program with highlighting and memory; extremely janky fmting.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::env;
 use std::fs;
 use std::io;
+use std::io::Write;
+use std::fmt;
 use std::path;
+use std::thread;
+use std::time::Duration;
 use std::process;
 
-// use termion;
+use termion::color;
+use itertools::{Itertools, EitherOrBoth};
 
 const MEM_SIZE: usize = 30_000;
 
 // Commands known to the VM
+#[derive(Debug)]
 enum Command {
     JumpForward(usize),
     JumpBackward(usize),
@@ -24,6 +30,7 @@ enum Command {
 // Parsed instruction with satellite data
 struct Instruction {
     command: Command,
+    ch: char,
     pos: (usize, usize),  // Screen position
 }
 
@@ -32,9 +39,8 @@ fn parse(chs: &Vec<char>) -> Result<Vec<Instruction>, ()> {
     let mut instructions: Vec<Instruction> = Vec::new();
     let mut brack_stack: Vec<usize> = Vec::new();
 
-    let (mut pos_x, mut pos_y): (usize, usize) = (0, 1);
+    let (mut pos_x, mut pos_y): (usize, usize) = (0, 0);
     for (i, ch) in chs.iter().enumerate() {
-        pos_x += 1;
         // Is this bad form?
         let command = match ch {
             '[' => {
@@ -55,8 +61,7 @@ fn parse(chs: &Vec<char>) -> Result<Vec<Instruction>, ()> {
             '+' => { Command::IncData },
             '.' => { Command::Output },
             ',' => { Command::Input },
-            '\n' => { pos_x = 0;
-                      pos_y += 1;
+            '\n' => { pos_y += 1;
                       Command:: NoOp
             }
             _ => { Command::NoOp },
@@ -65,48 +70,80 @@ fn parse(chs: &Vec<char>) -> Result<Vec<Instruction>, ()> {
         instructions.push(
             Instruction {
                 command: command,
+                ch: *ch,
                 pos: (pos_x, pos_y),
             }
         );
+
+        if *ch == '\n' {
+            pos_x = 0;
+        } else {
+            pos_x += 1;
+        }
     }
 
     Ok(instructions)
 }
 
+struct DisplaySpec {
+    visible: bool,
+    decimal: bool,
+    hex: bool,
+    ascii: bool,
+    frame_dur: Duration,
+}
+
+impl DisplaySpec {
+    fn new(rate: f32) -> DisplaySpec {
+        DisplaySpec {
+            visible: true,
+            decimal: true,
+            hex: true,
+            ascii: true,
+            frame_dur: Duration::from_millis((1000.0 / rate) as u64),
+        }
+    }
+}
+
 // Language virtual machine
 struct Machine {
-    prog_src: Vec<char>,
     prog: Vec<Instruction>,
     data: [u8; MEM_SIZE],
     prog_ctr: usize,
     data_ptr: usize,
 
-    visible: bool,
     last_data_cell: usize,
+    prog_src: Vec<String>,
+    display_spec: DisplaySpec,
 }
+
 
 impl Machine {
     fn new(program: String) -> Result<Machine, ()> {
-        let prog_src = program.chars().collect();
         let machine = Machine {
-            prog: parse(&prog_src)?,
-            prog_src: prog_src,
+            prog: parse(&program.chars().collect())?,
 
-            data: [0; ARRAY_SIZE],
+            data: [0; MEM_SIZE],
             prog_ctr: 0,
             data_ptr: 0,
 
+            prog_src: program.split('\n')
+                .map(|s| s.to_owned())
+                .collect(),
             last_data_cell: 0,
-            visible: false,
+            display_spec : DisplaySpec::new(1.0),
         };
 
         Ok(machine)
     }
 
+
     // Run the machine to termination.
     fn run(&mut self) {
-        if self.visible {
+        if self.display_spec.visible {
             loop {
+                println!("{}", self);
+                thread::sleep(self.display_spec.frame_dur);
                 self.advance();
             }
         } else {
@@ -118,11 +155,11 @@ impl Machine {
 
     // Advance to next non-noop command
     fn advance(&mut self) {
+        self.execute();
+        self.inc_prog_ctr();
         while let Command::NoOp = &self.prog[self.prog_ctr].command {
             self.inc_prog_ctr();
         }
-        self.execute();
-        self.inc_prog_ctr();
     }
 
     // Step forward or terminate
@@ -136,8 +173,8 @@ impl Machine {
     // Execute the command under the read head
     fn execute(&mut self) {
         match self.prog[self.prog_ctr].command {
-            Command::JumpForward(i) => { self.jmp_forward(i); },
-            Command::JumpBackward(i) => { self.jmp_backward(i); },
+            Command::JumpForward(i) => { self.jmp_eq(i); },
+            Command::JumpBackward(i) => { self.jmp_ne(i); },
             Command::DecPtr => { self.data_ptr -= 1; },
             Command::IncPtr => { self.data_ptr += 1; },
             Command::DecData => { self.dec_data(); },
@@ -185,6 +222,94 @@ impl Machine {
         self.data[self.data_ptr] += 1;
     }
 
+    // Returns a formatted data cell in decimal, hex, and ascii
+    // TODO This is pretty janky. I feel like I'm missing an abstraction here.
+    // Should I be using a custom formatter?
+    // TODO I'm not sure what the "right" place to put it is.
+    fn fmt_data_cell(&self, cell: usize) -> String {
+        let data = &self.data[cell];
+        let text = format!("{}{}{}",
+                           if self.display_spec.decimal {
+                               format!("{:03}", data)       // Decimal column
+                           } else {
+                               String::new()
+                           },
+
+                           if self.display_spec.hex {      // Hex column
+                               format!(" 0x{:02x}", data)
+                           } else {
+                               String::new()
+                           },
+
+                           if self.display_spec.ascii {    // Ascii  column
+                               // Printable ascii: is there a better way to do this?
+                               // Also, this gets DEL wrong
+                               format!(" {:}", if *data >= 0x20 as u8 { (*data as char) }
+                                       else { ' ' })
+                           } else {
+                               String::new()
+                           },
+        );
+        if cell == self.data_ptr {
+            format!("{}{}{}",
+                    color::Bg(color::Blue),
+                    text,
+                    color::Bg(color::Reset)
+            )
+        } else {
+            text
+        }
+    }
+
+    // Returns a formatted line of source code with read-head highlighting
+    fn fmt_src_line(&self, linum: usize) -> String {
+        let (pos_x, pos_y) = self.prog[self.prog_ctr].pos;
+        if linum == pos_y {
+            let (head, tail) = &self.prog_src[linum].split_at(pos_x);
+            let (ch, tail) = if tail.len() > 0 {
+                tail.split_at(1)
+            } else {
+                ("", "")
+            };
+            format!("{}{}{}{}{}",
+                    head,
+                    color::Bg(color::Blue),
+                    ch,
+                    color::Bg(color::Reset),
+                    tail)
+        } else {
+            self.prog_src[linum].clone()
+        }
+    }
+}
+
+impl fmt::Display for Machine {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let data_col_width = self.fmt_data_cell(0).len();
+        let repr = (0..=std::cmp::max(self.last_data_cell, self.data_ptr))  // Data column
+            // TODO should I put the `fmt_data_cell` here, or in the `match cols` below?
+            // .map(|x| self.fmt_data_cell(x))  // Format the left-hand column
+            .zip_longest(0..self.prog_src.len())     // Zip with source column
+            .map(|cols| {                            // Join the columns
+                match cols {
+                    EitherOrBoth::Both(cell, src) => {
+                        format!("{} {}\n", self.fmt_data_cell(cell),
+                                self.fmt_src_line(src))
+                    }
+                    EitherOrBoth::Left(cell) => {
+                        format!("{}\n", self.fmt_data_cell(cell))
+                    },
+                    EitherOrBoth::Right(src) => {
+                        format!("           {}\n",  // TODO this is a bug
+                                self.fmt_src_line(src))
+                                //width = data_col_width + 1)
+                    },
+                }
+            })
+            .collect::<String>();
+
+        write!(f, "{}", repr)
+    }
 }
 
 fn main() {
